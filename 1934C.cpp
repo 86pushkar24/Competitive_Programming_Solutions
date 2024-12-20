af80cd911542511a209ced093f0ab1198278c5a3
i-m-yc <guptapushkar86@gmail.com>
2024-12-20 23:09:58 +0530

    cf div-2

--- 1934C.cpp
+++ 1934C.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
 int ask(int x, int y)
 {
+    if(x<1 || y<1 || x>n || y>m) return 7;
     cout << "? " << x << " " << y << endl;
     int r;
     cin >> r;
@@ -14,20 +16,23 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, m;
         cin >> n >> m;
-        int d = 1 + ask(1, 1);
-        int f = ask(1, d);
-        f += f % 2;
-        int g = ask(1 + f / 2, d - f / 2);
-        int x = 1 + f / 2;
-        int y = d - f / 2;
-        g += g % 2;
-        int k = ask(x + g / 2, y - g / 2);
-        x += g / 2;
-        y -= g / 2;
-        x += k / 2;
-        y -= k / 2;
-        cout << "! " << x << " " << y << endl;
+        int d = ask(1, 1);
+        int d2 = ask(n, 1);
+        int x = n + d - d2 + 1;
+        x /= 2;
+        int y = d - x + 2;
+        int d3 = ask(n, m);
+        int x2 = 2 * n + m - 1 - d2 - d3;
+        x2 /= 2;
+        int y2 = d2 + x2 + 1 - n;
+        if (ask(x, y) == 0)
+        {
+            cout << "! " << x << " " << y << endl;
+        }
+        else
+        {
+            cout << "! " << x2 << " " << y2 << endl;
+        }
     }
 }
