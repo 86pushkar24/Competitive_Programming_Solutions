b48a5a766960354941f73d204f040c7533649232
86pushkar24 <guptapushkar86@gmail.com>
2025-02-12 22:46:49 +0530

    Add solutions for CodeChef problems: Cool Checker, Time Penalty, and Cool Subsequences

---
+++ Introverts.cpp
@@ -0,0 +1,188 @@
+/*                                          जय श्री राधे कृष्णा।                                                         */
+
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+using namespace __gnu_pbds;
+
+// Macros
+#define int long long
+#define endl '\n'
+#define for0(i,n)for(int i=0;i<n;++i)
+#define for1(i,n)for(int i=1;i<=n;++i)
+#define fl(a,b)for(int i=a;i<(b);++i)
+#define rfl(a,b)for(int i=a;i>=(b);--i)
+
+#define lbound(v, x) lower_bound(v.begin(),v.end(),x)-v.begin()
+#define ubound(v, x) upper_bound(v.begin(),v.end(),x)-v.begin()
+#define mne(v) *min_element(v.begin(),v.end())
+#define mxe(v) *max_element(v.begin(),v.end())
+
+#define yes cout<<"YES"<<endl;
+#define no cout << "NO" << endl;
+#define cy(x){if(x)yes else no}
+
+#define aint(v) v.begin(),v.end()
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pair<int, int>>
+#define pii pair<int, int>
+#define di deque<int>
+#define que queue<int>
+// #define si set<int>
+// #define mii map<int, int>
+#define mts multiset<int>
+#define mii fast_map<int, int>
+#define si fast_set <int>
+
+#define pb push_back
+#define bg begin()
+#define nd end()
+#define fi first
+#define se second
+#define ins insert
+#define mp make_pair
+
+// Input/Output Macros
+#define cin(a) int n;cin>>n;vi a(n);for0(i,n){cin>>a[i];}
+#define co(a) {cout<<a<<' ';}
+#define cou(a) {cout<<a<<"\n";}
+#define ci cin >>
+#define sz(c) c.size()
+
+// Vector Operations
+#define sortv(v) sort(aint(v))
+#define rev(v) reverse(aint(v))
+#define sumv(arr) accumulate(aint(arr), 0LL)
+#define Ceil(a, b) ((a + b - 1) / b)
+
+// Type Aliases for Nested Containers
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvi = vector<vector<int>>;
+using vvp = vector<vector<pair<int, int>>>;
+
+// Constants
+const int maxn = 4e5 + 5;
+const int inf = 1e18;
+const int mod = 1e9 + 7;
+
+
+// Custom Hash Function (for preventing collisions in hashing)
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}
+    size_t operator()(uint64_t x) const{static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
+template <typename K, typename V> using fast_map = gp_hash_table<K, V, custom_hash>;
+template <typename K>  using fast_set = gp_hash_table<K, null_type, custom_hash>;
+// Utility Functions (Commented for Optional Use)
+// int gcd(int a, int b) { return a ? gcd(b % a, a) : b; }
+// int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+// int binpow(int x, int y, int m) { int res(1); x = x % m; while (y > 0) { if (y & 1) res = (res * x) % m; y = y >> 1; x = (x * x) % m; } return res; }
+// int qexp(int a, int b, int m) { int res(1); while (b) { if (b % 2) res = res * a % m; a = a * a % m; b /= 2; } return res; }
+// bool isPrime(int n) { if (n <= 1) return false; for (int i = 2; i * i <= n; i++) { if (n % i == 0) return false; } return true; }
+// static bool cmp(const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; }
+
+// Pushkar Gupta's Solution Starts Here
+vi fn(int lt,int rt,int n){
+    vi v;
+    if(lt==0 && rt==n+1){
+        v.pb(1);
+        v.pb(n);
+        return v;
+    }
+    if(lt==0){
+        v.pb(1);
+        return v;
+    }
+    if(rt==n+1){
+        v.pb(n);
+        return v;
+    }
+    int k=rt-lt;
+    if(k%2!=1) v.pb((lt+rt)/2);
+    else{
+        v.pb(lt+k/2);
+        v.pb(lt+k/2+1);
+    }
+    return v;
+}
+void push(){
+    int n;
+    ci n;
+    vi v0(n+1);
+    for1(i,n){
+        int p;
+        ci p;
+        v0[p]=i;
+    }
+    int ans(1);
+    set<pii> seg;
+    mts mt;
+    seg.ins({0,n+1});
+    auto gt=[&](int lt,int rt,int n)->int {
+        if(lt==0 && rt==n+1) return n;
+        if(lt==0) return rt-1;
+        if(rt==n+1) return n-lt;
+        return (rt-lt)/2;
+    };
+    mt.ins(gt(0,n+1,n));
+    for1(i,n){
+        if(!ans) break;
+        int k=v0[i];
+        auto a1=seg.upper_bound({k,inf});
+        if(a1 == seg.bg){
+            ans=0;
+            break;
+        }
+        a1--;
+        auto [lt,rt]=*a1;
+        if(!(lt<k && k<rt)){
+            ans=0;
+            break;
+        }
+        int t=gt(lt,rt,n);
+        if(t != *mt.rbegin()){
+            ans=0;
+            break;
+        }
+        vi v1=fn(lt,rt,n);
+        if(find(aint(v1),k)==v1.nd){
+            ans=0;
+            break;
+        }
+        seg.erase(a1);
+        auto a2=mt.find(t);
+        if(a2!=mt.nd) mt.erase(a2);
+        if(k-lt>1){
+            seg.ins({lt,k});
+            mt.ins(gt(lt,k,n));
+        }
+        if(rt-k>1){
+            seg.ins({k,rt});
+            mt.ins(gt(k,rt,n));
+        }
+    }
+    cy(ans);
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        push();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds." << endl;
+}
---
+++ Time_Penalty.cpp
@@ -0,0 +1,112 @@
+/*                                          जय श्री राधे कृष्णा।                                                         */
+
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+using namespace __gnu_pbds;
+
+// Macros
+#define int long long
+#define endl '\n'
+#define for0(i,n)for(int i=0;i<n;++i)
+#define for1(i,n)for(int i=1;i<=n;++i)
+#define fl(a,b)for(int i=a;i<(b);++i)
+#define rfl(a,b)for(int i=a;i>=(b);--i)
+
+#define lbound(v, x) lower_bound(v.begin(),v.end(),x)-v.begin()
+#define ubound(v, x) upper_bound(v.begin(),v.end(),x)-v.begin()
+#define mne(v) *min_element(v.begin(),v.end())
+#define mxe(v) *max_element(v.begin(),v.end())
+
+#define yes cout<<"YES"<<endl;
+#define no cout << "NO" << endl;
+#define cy(x){if(x)yes else no}
+
+#define aint(v) v.begin(),v.end()
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pair<int, int>>
+#define pii pair<int, int>
+#define di deque<int>
+#define que queue<int>
+// #define si set<int>
+// #define mii map<int, int>
+#define mts multiset<int>
+#define mii fast_map<int, int>
+#define si fast_set <int>
+
+#define pb push_back
+#define bg begin()
+#define nd end()
+#define fi first
+#define se second
+#define ins insert
+#define mp make_pair
+
+// Input/Output Macros
+#define cin(a) int n;cin>>n;vi a(n);for0(i,n){cin>>a[i];}
+#define co(a) {cout<<a<<' ';}
+#define cou(a) {cout<<a<<"\n";}
+#define ci cin >>
+#define sz(c) c.size()
+
+// Vector Operations
+#define sortv(v) sort(aint(v))
+#define rev(v) reverse(aint(v))
+#define sumv(arr) accumulate(aint(arr), 0LL)
+#define Ceil(a, b) ((a + b - 1) / b)
+
+// Type Aliases for Nested Containers
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvi = vector<vector<int>>;
+using vvp = vector<vector<pair<int, int>>>;
+
+// Constants
+const int maxn = 4e5 + 5;
+const int inf = 1e18;
+const int mod = 1e9 + 7;
+
+
+// Custom Hash Function (for preventing collisions in hashing)
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}
+    size_t operator()(uint64_t x) const{static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
+template <typename K, typename V> using fast_map = gp_hash_table<K, V, custom_hash>;
+template <typename K>  using fast_set = gp_hash_table<K, null_type, custom_hash>;
+// Utility Functions (Commented for Optional Use)
+// int gcd(int a, int b) { return a ? gcd(b % a, a) : b; }
+// int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+// int binpow(int x, int y, int m) { int res(1); x = x % m; while (y > 0) { if (y & 1) res = (res * x) % m; y = y >> 1; x = (x * x) % m; } return res; }
+// int qexp(int a, int b, int m) { int res(1); while (b) { if (b % 2) res = res * a % m; a = a * a % m; b /= 2; } return res; }
+// bool isPrime(int n) { if (n <= 1) return false; for (int i = 2; i * i <= n; i++) { if (n % i == 0) return false; } return true; }
+// static bool cmp(const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; }
+
+// Pushkar Gupta's Solution Starts Here
+void push(){
+    int a,b;
+    ci a>>b;
+    cou(a+10*b)
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    int tc = 1;
+    // cin >> tc;
+
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        push();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds." << endl;
+}
