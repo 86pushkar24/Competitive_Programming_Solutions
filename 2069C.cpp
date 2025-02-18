75a9f59e9f4b6273cee529898ec4447045d4e161
86pushkar24 <guptapushkar86@gmail.com>
2025-02-19 00:54:21 +0530

    Add solutions for Codeforces problems 2069A, 2069B, 2069C, and 2069D

---
+++ 2069C.cpp
@@ -0,0 +1,200 @@
+/*                                          जय श्री राधे कृष्णा।                                                         */
+
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+using namespace __gnu_pbds;
+
+// Macros
+// #define int long long
+#define endl '\n'
+#define for0(i,n)for(int i=0;i<n;++i)
+#define for1(i,n)for(int i=1;i<=n;++i)
+#define fl(a,b)for(int i=a;i<(b);++i)
+#define rfl(a,b)for(int i=a;i>=(b);--i)
+
+#define lbound(v, x) lower_bound(v.begin(),v.end(),x)-v.begin()
+#define ubound(v, x) upper_bound(v.begin(),v.end(),x)-v.begin()
+#define mne(v) *min_element(v.begin(),v.end())
+#define mxe(v) *max_element(v.begin(),v.end())
+
+#define yes cout<<"YES"<<endl;
+#define no cout << "NO" << endl;
+#define cy(x){if(x)yes else no}
+
+#define aint(v) v.begin(),v.end()
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vpii vector<pair<int, int>>
+#define pii pair<int, int>
+#define di deque<int>
+#define que queue<int>
+// #define si set<int>
+// #define mii map<int, int>
+#define mts multiset<int>
+#define mii fast_map<int, int>
+#define si fast_set <int>
+
+#define pb push_back
+#define bg begin()
+#define nd end()
+#define fi first
+#define se second
+#define ins insert
+#define mp make_pair
+
+// Input/Output Macros
+#define cin(a) int n;cin>>n;vi a(n);for0(i,n){cin>>a[i];}
+#define co(a) {cout<<a<<' ';}
+#define cou(a) {cout<<a<<"\n";}
+#define ci cin >>
+#define sz(c) c.size()
+
+// Vector Operations
+#define sortv(v) sort(aint(v))
+#define rev(v) reverse(aint(v))
+#define sumv(arr) accumulate(aint(arr), 0LL)
+#define Ceil(a, b) ((a + b - 1) / b)
+
+// Type Aliases for Nested Containers
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvi = vector<vector<int>>;
+using vvp = vector<vector<pair<int, int>>>;
+
+// Constants
+const int maxn = 4e5 + 5;
+const int inf = 1e18;
+const int mod = 1e9 + 7;
+const int N = 2e5 + 5;
+
+// Custom Hash Function (for preventing collisions in hashing)
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}
+    size_t operator()(uint64_t x) const{static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
+template <typename K, typename V> using fast_map = gp_hash_table<K, V, custom_hash>;
+template <typename K>  using fast_set = gp_hash_table<K, null_type, custom_hash>;
+// Utility Functions (Commented for Optional Use)
+// int gcd(int a, int b) { return a ? gcd(b % a, a) : b; }
+// int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+int binpow(int x, int y, int m) { int res(1); x = x % m; while (y > 0) { if (y & 1) res = (res * x) % m; y = y >> 1; x = (x * x) % m; } return res; }
+// int qexp(int a, int b, int m) { int res(1); while (b) { if (b % 2) res = res * a % m; a = a * a % m; b /= 2; } return res; }
+// bool isPrime(int n) { if (n <= 1) return false; for (int i = 2; i * i <= n; i++) { if (n % i == 0) return false; } return true; }
+// static bool cmp(const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; }
+
+// Pushkar Gupta's Solution Starts Here
+template <const int T>
+struct ModInt
+{
+    const static int mod = T;
+    int x;
+    ModInt(int x = 0) : x(x % mod) {}
+    ModInt(long long x) : x(int(x % mod)) {}
+    int val() { return x; }
+    ModInt operator+(const ModInt &a) const
+    {
+        int x0 = x + a.x;
+        return ModInt(x0 < mod ? x0 : x0 - mod);
+    }
+    ModInt operator-(const ModInt &a) const
+    {
+        int x0 = x - a.x;
+        return ModInt(x0 < 0 ? x0 + mod : x0);
+    }
+    ModInt operator*(const ModInt &a) const { return ModInt(1LL * x * a.x % mod); }
+    ModInt operator/(const ModInt &a) const { return *this * a.inv(); }
+    bool operator==(const ModInt &a) const { return x == a.x; };
+    bool operator!=(const ModInt &a) const { return x != a.x; };
+    void operator+=(const ModInt &a)
+    {
+        x += a.x;
+        if (x >= mod)
+            x -= mod;
+    }
+    void operator-=(const ModInt &a)
+    {
+        x -= a.x;
+        if (x < 0)
+            x += mod;
+    }
+    void operator*=(const ModInt &a) { x = 1LL * x * a.x % mod; }
+    void operator/=(const ModInt &a) { *this = *this / a; }
+    friend ModInt operator+(int y, const ModInt &a)
+    {
+        int x0 = y + a.x;
+        return ModInt(x0 < mod ? x0 : x0 - mod);
+    }
+    friend ModInt operator-(int y, const ModInt &a)
+    {
+        int x0 = y - a.x;
+        return ModInt(x0 < 0 ? x0 + mod : x0);
+    }
+    friend ModInt operator*(int y, const ModInt &a) { return ModInt(1LL * y * a.x % mod); }
+    friend ModInt operator/(int y, const ModInt &a) { return ModInt(y) / a; }
+    friend ostream &operator<<(ostream &os, const ModInt &a) { return os << a.x; }
+    friend istream &operator>>(istream &is, ModInt &t) { return is >> t.x; }
+
+    ModInt pow(int64_t n) const
+    {
+        ModInt res(1), mul(x);
+        while (n)
+        {
+            if (n & 1)
+                res *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    ModInt inv() const
+    {
+        int a = x, b = mod, u = 1, v = 0;
+        while (b)
+        {
+            int t = a / b;
+            a -= t * b;
+            swap(a, b);
+            u -= t * v;
+            swap(u, v);
+        }
+        if (u < 0)
+            u += mod;
+        return u;
+    }
+};
+using mint=ModInt<998244353>;
+void push(){
+    int n;
+    ci n;
+    vi v(n+1);
+    for1(i,n)ci v[i];
+    mint ans(0),cnt(0),cnt1(0);
+    for1(i,n){
+        if(v[i]==1)cnt+=1,cnt1+=1;
+        else if(v[i]==2)cnt*=2;
+        else ans+=(cnt-cnt1);
+    }
+    cou(ans)
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(0);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+    int tc = 1;
+    cin >> tc;
+
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        push();
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds." << endl;
+}
