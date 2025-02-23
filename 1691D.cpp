c4537f3a32ef8dba2523d0a201309c36861202ee
86pushkar24 <guptapushkar86@gmail.com>
2025-02-23 12:37:44 +0530

    Refactor formatting in solution for Codeforces problem 1691D; improve code readability

--- 1691D.cpp
+++ 1691D.cpp
@@ -1,16 +1,16 @@
 #include "bits/stdc++.h"
 using namespace std;
-bool chk(vector<int>&v) {
-    int cur=0, n=v.size();
+bool chk(vector<int> &v){
+    int cur=0,n=v.size();
     int mx=LLONG_MIN;
-    for (int i=0; i<n; i++) {
+    for (int i=0;i<n;i++){
         if (cur<=0){ 
             cur=0;
             mx=v[i];
         }
         cur+=v[i];
         mx=max(mx, v[i]);
-        if (cur>mx) return false;
+        if(cur>mx)return false;
     }
     return true;
 }
@@ -18,14 +18,14 @@ void push() {
     int n;
     cin>>n;
     vector<int> v(n);
-    for (auto&it : v) cin>>it;
+    for (auto&it:v)cin>>it;
     vector<int> a=v;
-    sort(a.begin(), a.end());
+    sort(a.begin(),a.end());
     cout<<(chk(v)||chk(a)?"YES":"NO")<<'\n';
 }
 int main() {
     int tc;
     cin>>tc;
-    while (tc--) push();
+    while (tc--)push();
     return 0;
 }
