f20f65819d7b721536d28673e84fbc0cf4e15bbe
86pushkar24 <guptapushkar86@gmail.com>
2025-02-26 14:50:35 +0530

    Fix logic in solution for Codeforces problem 1797C; handle edge cases for single row/column scenarios and improve output handling

--- 1797C.cpp
+++ 1797C.cpp
@@ -13,10 +13,28 @@ int main(){
     while(tc--){
         int n,m;
         cin>>n>>m;
+        if(n==1||m==1){
+            if(n==1){
+            int c=query(1,1);
+            cout<<"! "<<1<<" "<<c+1<<endl;
+            cout<<flush;
+            }
+            else{
+            int r=query(1,1);
+            cout<<"! "<<r+1<<" "<<1<<endl;
+            cout<<flush;
+            }
+            continue;
+        }
         vector<int> v(3);
         v[0]=query(1,1);
         v[1]=query(1,2);
         int r(0),c(0);
+        if(v[0] == 0){
+        cout<<"! "<<1<<" "<<1<<endl;
+        cout<<flush;
+        continue;
+        }
         if(v[1]<v[0]){
             c=v[0]+1;
             v[2]=query(1,c);
