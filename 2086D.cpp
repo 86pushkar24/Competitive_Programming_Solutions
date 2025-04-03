97193eb26e6bb955da78db28638b4fd63c7eeea4
86pushkar24 <guptapushkar86@gmail.com>
2025-04-04 00:01:15 +0530

    Update problem data for B. Large Array and Segments; refactor solution in 2086D.cpp

--- 2086D.cpp
+++ 2086D.cpp
@@ -1,145 +1,69 @@
-/*                                          जय श्री राधे कृष्णा।                                                         */
-
-#include "bits/stdc++.h"
-#include "ext/pb_ds/assoc_container.hpp"
+#include<bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
 
-// Macros
 #define int long long
-#define endl '\n'
-#define for0(i,n)for(int i=0;i<n;++i)
-#define for1(i,n)for(int i=1;i<=n;++i)
-#define fl(a,b)for(int i=a;i<(b);++i)
-#define rfl(a,b)for(int i=a;i>=(b);--i)
+const int mod=998244353;
+const int maxn=2e6+5;
 
-#define lbound(v, x) lower_bound(v.begin(),v.end(),x)-v.begin()
-#define ubound(v, x) upper_bound(v.begin(),v.end(),x)-v.begin()
-#define mne(v) *min_element(v.begin(),v.end())
-#define mxe(v) *max_element(v.begin(),v.end())
-
-#define yes cout<<"YES"<<endl;
-#define no cout << "NO" << endl;
-#define cy(x){if(x)yes else no}
-
-#define aint(v) v.begin(),v.end()
-#define vi vector<int>
-#define vb vector<bool>
-#define vc vector<char>
-#define vs vector<string>
-#define vpii vector<pair<int, int>>
-#define pii pair<int, int>
-#define di deque<int>
-#define que queue<int>
-// #define si set<int>
-// #define mii map<int, int>
-#define mts multiset<int>
-#define mii fast_map<int, int>
-#define si fast_set <int>
-
-#define pb push_back
-#define bg begin()
-#define nd end()
-#define fi first
-#define se second
-#define ins insert
-#define mp make_pair
-
-// Input/Output Macros
-#define cin(a) int n;cin>>n;vi a(n);for0(i,n){cin>>a[i];}
-#define co(a) {cout<<a<<' ';}
-#define cou(a) {cout<<a<<"\n";}
-#define ci cin >>
-#define sz(c) c.size()
-
-// Vector Operations
-#define sortv(v) sort(aint(v))
-#define rev(v) reverse(aint(v))
-#define sumv(arr) accumulate(aint(arr), 0LL)
-#define Ceil(a, b) ((a + b - 1) / b)
-
-// Type Aliases for Nested Containers
-using vvb = vector<vector<bool>>;
-using vvc = vector<vector<char>>;
-using vvi = vector<vector<int>>;
-using vvp = vector<vector<pair<int, int>>>;
-
-// Constants
-const int maxn = 500001;
-const int inf = 1e18;
-const int mod = 998244353;
-
-// Custom Hash Function (for preventing collisions in hashing)
-struct custom_hash{
-    static uint64_t splitmix64(uint64_t x){x += 0x9e3779b97f4a7c15;x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;x = (x ^ (x >> 27)) * 0x94d049bb133111eb;return x ^ (x >> 31);}
-    size_t operator()(uint64_t x) const{static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x + FIXED_RANDOM);}};
-template <typename K, typename V> using fast_map = gp_hash_table<K, V, custom_hash>;
-template <typename K>  using fast_set = gp_hash_table<K, null_type, custom_hash>;
-// Utility Functions (Commented for Optional Use)
-// int gcd(int a, int b) { return a ? gcd(b % a, a) : b; }
-// int lcm(int a, int b) { return (a * b) / gcd(a, b); }
-int binpow(int x, int y, int m) { int res(1); x = x % m; while (y > 0) { if (y & 1) res = (res * x) % m; y = y >> 1; x = (x * x) % m; } return res; }
-// int qexp(int a, int b, int m) { int res(1); while (b) { if (b % 2) res = res * a % m; a = a * a % m; b /= 2; } return res; }
-// bool isPrime(int n) { if (n <= 1) return false; for (int i = 2; i * i <= n; i++) { if (n % i == 0) return false; } return true; }
-// static bool cmp(const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; }
-
-// Pushkar Gupta's Solution Starts Here
-vi f(maxn),inv(maxn);
-void pre(){
+int power(int x,int y,int p){
+    int ans=1;
+    x%=p;
+    while(y>0){
+        if(y&1) ans=(ans*x)%p;
+        y>>=1;
+        x=(x*x)%p;
+    }
+    return ans;
+}
+int modInverse(int n,int p){
+    return power(n,p-2,p);
+}
+int f[maxn],inv[maxn];
+void pre(int n,int p){
     f[0]=1;
-    for1(i,maxn-1)f[i]=f[i-1]*i%mod;
-    inv[maxn-1]=binpow(f[maxn-1],mod-2,mod);
-    rfl(maxn-2,0)inv[i]=inv[i+1]*(i+1)%mod;
+    for(int i=1;i<=n;i++) f[i]=(f[i-1]*i)%p;
+    inv[n]=modInverse(f[n],p);
+    for(int i=n-1;i>=0;i--) inv[i]=inv[i+1]*(i+1)%p;
 }
+void solve(){
+    vector<int> v1(26,0);
+    for(int i=0;i<26;i++) cin>>v1[i];
+    int n=accumulate(v1.begin(),v1.end(),0ll);
+    vector<int> v2;
+    for(int i=0;i<26;i++) if(v1[i]>0) v2.push_back(v1[i]);
+    int odd=(n+1)/2,even=n-odd;
+    auto fn=[&](int l,int r){
+        int len=r-l+1;
+        vector<int> ans;
+        for(int i=0;i<(1<<len);i++){
+            int sum=0;
+            for(int j=0;j<len;j++) if(i&(1<<j)) sum+=v2[l+j];
+            ans.push_back(sum);
+        }
+        return ans;
+    };
+    int z=v2.size();
+    if(z==1){
+        cout<<(v2[0]==1?1:0)<<endl;
+        return;
+    }
+    vector<int> left=fn(0,(z/2)-1);
+    vector<int> right=fn((z/2),z-1);
+    sort(left.begin(),left.end());
+    sort(right.begin(),right.end());
+    int ans=0,mult=(f[odd]*f[even])%mod;
+    for(int i:v2) mult=(mult*inv[i])%mod;
+    for(int i:left){
+        int l=lower_bound(right.begin(),right.end(),odd-i)-right.begin();
+        int r=upper_bound(right.begin(),right.end(),odd-i)-right.begin();
+        ans=(ans+((r-l)*mult)%mod)%mod;
+    }
 
-void push(){
-    vi v1,v2(26);
-    for0(i,26)ci v2[i];
-    int n=sumv(v2),eve=(n+1)/2,odd=n/2;
-    int k(0);
-    for0(i,26){
-        if(v2[i]>1)v1.pb(i);
-        else if(v2[i]==1)k++;
-    }
-    int ans(0),asz=sz(v1);
-    for0(i,(1<<asz)){
-        int sme(0),sm0(0);
-        for0(j,asz){
-            int idx=v1[j];
-            if(i&(1<<j))sme+=v2[idx];
-            else sm0+=v2[idx];
-        }
-        int eprm=eve-sme,oprm=odd-sm0;
-        if(eprm<0||oprm<0||eprm+oprm!=k)continue;
-        int bn=f[k]*inv[eprm]%mod*inv[k-eprm]%mod;
-        int ednm=1,odnm=1;
-        for0(j,asz){
-            int idx=v1[j];
-            if(i&(1<<j))ednm=ednm*inv[v2[idx]]%mod;
-            else odnm=odnm*inv[v2[idx]]%mod;
-        }
-        int k=bn*f[eve]%mod*ednm%mod*f[odd]%mod*odnm%mod;
-        ans=(ans+k)%mod;
-    }
-    cou(ans)
+    cout<<ans<<endl;
 }
-
-signed main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(0);
-
-    auto begin = std::chrono::high_resolution_clock::now();
-    pre();
-    int tc = 1;
-    cin >> tc;
-
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
-        push();
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds." << endl;
+signed main(){
+    int t;
+    cin>>t;
+    pre(maxn-1,mod);
+    while(t--) solve();
 }
